0cbcc1f002a057a2eafabf474c72b11675680687
Clayton Roberts <Claymasterj@wavewise.us>
2019-01-02 20:56:46 -0800

    Pre-Launch Commit

--- static/src/index.js
+++ static/src/index.js
@@ -2,16 +2,16 @@ import './index.scss';
 import JQuery from 'jquery';
 import Bootstrap from 'bootstrap';
 import Vue from 'vue';
+import _ from 'lodash';
 // import FontAwesome from 'fontawesome-free';
 // require("~@fortawesome/fontawesome-free/all.js");
-// require("font-awesome-webpack");
-// require("font-awesome-loader");
-// require("font-awesome-webpack");
-// import "font-awesome-loader";
-// import "font-awesome";
+// require("font-awesome-webpack"); require("font-awesome-loader");
+// require("font-awesome-webpack"); import "font-awesome-loader"; import
+// "font-awesome";
+import VueScrollTo from 'vue-scrollto';
+
+Vue.use(VueScrollTo)
 window.Vue = Vue;
-// import Vuex from 'vuex';
-// Vue.use(Vuex)
-// import fontawesome;
-// const Vue = require('node_modules/vue');
+// import Vuex from 'vuex'; Vue.use(Vuex) import fontawesome; const Vue =
+// require('node_modules/vue');
 //
