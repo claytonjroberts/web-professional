9c9a3574d13a6b26e863c38b6c447ceeaf75e4e9
Clayton J Roberts <claytonjroberts@icloud.com>
2021-10-30 00:53:13 -0400

    Added environment varible 'PORT' for heroku

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # This file needs to be not named __main__ because it causes issues with Gunicorn
 
+import os
 from personal.web.app_tornado import App
 
 app = App()
 
 if __name__ == "__main__":
-    app.serve()
+    app.serve(port=int(os.environ["PORT"]))
